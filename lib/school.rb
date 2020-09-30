b8805ee75ad07020449b6e05f5d95e0be4053593
imperative-scientist-4600 <nelom3@gmail.com>
2020-09-30 21:24:22 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,32 +1,62 @@
-# code here!
+# # code here!
+# class School
+  
+#   def initialize(name)
+#     @name = name
+#     @roster = {}
+#   end
+
+#   def roster
+#     @roster
+#   end
+ 
+#   def add_student(name, grade)
+#     if roster.keys.include?(grade)
+#       roster[grade] << name
+#     else
+#       roster[grade] = []
+#       roster[grade] << name
+#     end
+#   end
+ 
+#   def grade(grade)
+#     roster[grade]
+#   end
+
+#   def sort
+#     roster.each do |key, value|
+#       roster[key] = value.sort!
+#     end
+#   end
+
+# end
+
+
 class School
+  
+  attr_accessor :roster
   
   def initialize(name)
     @name = name
     @roster = {}
   end
-
-  def roster
-    @roster
-  end
- 
+  
   def add_student(name, grade)
-    if roster.keys.include?(grade)
+    if roster[grade]
       roster[grade] << name
     else
       roster[grade] = []
       roster[grade] << name
     end
   end
- 
+  
   def grade(grade)
     roster[grade]
   end
-
+  
   def sort
-    roster.each do |key, value|
-      roster[key] = value.sort!
-    end
+    roster.map{|key, value| value.sort}
   end
-
+  
 end
+
